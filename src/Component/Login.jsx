c8e02b64d52fdd8ae946c45130c0f8e5bbeacb04
Kishore-L13 <kishore.krrish13@gmail.com>
2026-05-14 16:42:43 +0530

    Validator

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,9 +1,19 @@
 import Header from "./Header";
-import React, { useState } from "react"; // Don't forget to import useState
+import React, { useState,useRef } from "react";
+import { Validate } from "../Utilis/Validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
-
+  const [errormessage , setErrormessage] = useState(null);
+  const isEmail = useRef(null);
+  const isName = useRef(null);
+  const isPassword = useRef(null);
+const Handlevalidate = ()=>{
+  const email = isEmail.current.value;
+  const password = isPassword.current.value;
+const message =Validate(email,password);
+setErrormessage(message);
+}
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
@@ -14,7 +24,7 @@ const Login = () => {
              <img src="https://assets.nflxext.com/ffe/siteui/vlv3/76c5a455-c62c-46d4-8653-3924728113e3/web/IN-en-20260504-TRIFECTA-perspective_596176fe-3b1e-48ec-8a00-a0acb34e68f1_medium.jpg"
              alt="background"
        /></div>
-    <form className="absolute top-1/2 left-1/2 -translate-x-1/2 
+    <form onSubmit={(e)=>e.preventDefault()} className="absolute top-1/2 left-1/2 -translate-x-1/2 
     -translate-y-1/2 bg-black/80 p-12 w-full max-w-md rounded-lg text-white">
       <h1 className="text-3xl font-bold mb-8">
         {isSignInForm ? "Sign In" : "Sign Up"}
@@ -22,18 +32,20 @@ const Login = () => {
       {!isSignInForm && (
         <input 
           type="text" 
+          ref={isName}
           placeholder="Full Name" 
           className="p-4 my-2 w-full bg-gray-700 rounded-md focus:outline-none"
         />
       )}
 
-      <input type="text" placeholder="Email Address" className="p-4 my-2 w-full bg-gray-700 rounded-md" />
-      <input type="password" placeholder="Password" className="p-4 my-2 w-full bg-gray-700 rounded-md" />
+      <input type="text" ref={isEmail} placeholder="Email Address" className="p-4 my-2 w-full bg-gray-700 rounded-md" />
+      <input type="password" ref={isPassword} placeholder="Password" className="p-4 my-2 w-full bg-gray-700 rounded-md" />
 
-      <button className="p-4 my-6 w-full bg-red-600 rounded-md font-bold">
+      <button  className="p-4 my-6 w-full bg-red-600 rounded-md font-bold" onClick={()=>Handlevalidate()}>
         {isSignInForm ? "Sign In" : "Sign Up"}
         
       </button>
+      <p className="text-red-400 font-bold">{errormessage}</p>
         <div className="flex justify-between text-sm text-gray-400">
           <div>
             <input type="checkbox" className="mr-1" />
---
+++ src/Utilis/Validate.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export const Validate = (email,password)=>{
+    const isEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+    const isPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    if (!isEmail) return "Email is not valid";
+    if (!isPassword) return "Password invalid";
+    return null;
+}
